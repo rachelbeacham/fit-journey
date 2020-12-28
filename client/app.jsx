95b9e14f7947d7ede22532b0df64816fef2bfc7e
Rachel Beacham <rachelbeacham17@gmail.com>
2020-12-28 23:04:48 +0000

    Define a compnent that renders a modal inclding the exercise name, demo image, and how to description. #3

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ExerciseList from './pages/exercise-list';
 import ExerciseDetail from './components/exercise-detail';
+// import ExerciseList from './pages/exercise-list';
 // import Home from './pages/home';
 
 export default class App extends React.Component {
 
   render() {
-    return <ExerciseList />;
+    return <ExerciseDetail />;
   }
 }
--- client/components/exercise-detail.jsx
+++ client/components/exercise-detail.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 class ExerciseDetail extends React.Component {
   render() {
     return (
-
-    )
+      <div className="w-75 mt-5 container pop-out-colors">
+        <h3 className="mt-3 text-center text-white">Barbell Bench Press</h3>
+        <div className="text-center">
+          <img className="w-75" src="images/barbell-chest-press.jpeg" alt="demo image"></img>
+        </div>
+        <h4 className="mt-3 text-center text-white">How To:</h4>
+        <p className="text-white text-md-center text-left">
+          1. Lie on the bench with you eyes under the bar. <br />
+          2. Grab the bar with a medium grip-width (thumbs around the bar!) <br />
+          3. Unrack the bar by straigtenting your arms. <br />
+          4. Lower the bar to your mid chest. <br />
+          5. Press the bar back up until your arms are straight. <br />
+        </p>
+        <div className="text-center">
+          <button className="text-center white-text my-3">Done</button>
+        </div>
+      </div>
+    );
   }
 }
 
