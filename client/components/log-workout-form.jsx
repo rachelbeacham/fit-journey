2754f82a76c579c82af17531b5fc08fa137da15f
Rachel Beacham <rachelbeacham17@gmail.com>
2020-12-29 21:10:23 +0000

    Create a component that renders the first page of the log workout form. #4

--- client/components/log-workout-form.jsx
+++ client/components/log-workout-form.jsx
@@ -13,6 +13,15 @@ class LogWorkoutForm extends React.Component {
     return (
       <>
        <Header button='Discard' heading='Log a Workout' />
+       <form className="row">
+        <div className="flex-column justify-content-center">
+          <label htmlFor="date" className="form-label my-2 text-white">Date</label>
+          <input type="date" id="date" className="form-control pop-in-colors w-75 d-block"></input>
+          <label htmlFor="duration" className="form-label my-2 text-white">Duration</label>
+          <input type="text" id="duration" className="form-control pop-in-colors w-75 d-block"></input>
+          <button type="submit" className="green-button py-2 w-75">Add Exercises</button>
+        </div>
+       </form>
       </>
     );
   }
