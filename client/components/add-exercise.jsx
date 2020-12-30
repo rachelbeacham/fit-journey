5c056031e0c696966a495a794c804e9a4d2050fc
Rachel Beacham <rachelbeacham17@gmail.com>
2020-12-30 04:38:40 +0000

    Style to match wireframe. #4

--- client/components/add-exercise.jsx
+++ client/components/add-exercise.jsx
@@ -29,14 +29,14 @@ class AddExercise extends React.Component {
             <p className="fs-2 gray-text">{this.state.setCount}</p>
           </div>
           <div className="col text-center">
-            <input type="number" className="w-100 pop-in-colors"></input>
+            <input type="number" className="w-100 w-md-50 h-75 pop-in-colors"></input>
           </div>
           <div className="col text-center">
-            <input type="number" className="w-100 pop-in-colors"></input>
+            <input type="number" className="w-100 w-md-50 h-75 pop-in-colors"></input>
           </div>
         </div>
         <div className="d-flex justify-content-end">
-          <p className="fs-2 green-text">+Add a set</p>
+          <p className="fs-3 me-3 green-text">+Add a set</p>
         </div>
         <div className="text-center my-3">
           <button onClick={this.props.handleClick} className="text-center px-4 fs-4 py-2 green-button">Add to Workout</button>
