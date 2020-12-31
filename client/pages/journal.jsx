6c968ac41f180efb0b0ed733b3a13c1abc6b6143
Rachel Beacham <rachelbeacham17@gmail.com>
2020-12-31 23:18:23 +0000

    Style to match wireframe. #5

--- client/pages/journal.jsx
+++ client/pages/journal.jsx
@@ -2,34 +2,53 @@ import React from 'react';
 import Header from '../components/header';
 
 class JournalPage extends React.Component {
+  journalFull() {
+    return (
+      <>
+      <div className="row justify-content-evenly">
+        <div className="col">
+          <div className="pop-out-colors  lh-1 text-center">
+            <p className="text-white mt-2">Date:</p>
+            <p className="green-text">12/23/2020</p>
+            <p className="text-white">Duration:</p>
+            <p className="green-text">1 hour 30 mins</p>
+            <p className="mb-1 text-light text-decoration-underline">See Full Workout</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="pop-out-colors wh-75 lh-1 text-center">
+            <p className="text-white mt-2">Date:</p>
+            <p className="green-text">12/20/2020</p>
+            <p className="text-white">Duration:</p>
+            <p className="green-text">1 hour</p>
+            <p className="mb-1 text-light text-decoration-underline">See Full Workout</p>
+          </div>
+        </div>
+      </div>
+      <button className="position-fixed bottom-0 start-50 translate-middle-x my-3 green-button w-75 fs-2 py-2">
+        Log a Workout
+      </button>
+      </>
+    );
+  }
+
+  journalEmpty() {
+    return (
+      <>
+        <p className="text-center fs-3 text-muted mt-2">You havent logged any workouts yet!</p>
+        <button className="position-fixed bottom-0 start-50 translate-middle-x my-3 green-button w-75 fs-2 py-2">
+          Log a Workout
+        </button>
+      </>
+    );
+  }
+
   render() {
     return (
     <>
     <Header button='Back' heading='Logged Workouts' />
     <div className="container mt-3">
-      <div className="row justify-content-evenly">
-        <div className="col">
-          <div className="pop-out-colors lh-1 text-center">
-            <p className="text-white mt-2">Date:</p>
-            <p className="green-text">12/20/2020</p>
-            <p className="text-white">Duration:</p>
-            <p className="green-text">1 hour 30 minutes</p>
-            <p className="green-text text-decoration-underline">See Full Workout</p>
-          </div>
-        </div>
-        <div className="col">
-          <div className="pop-out-colors lh-1 text-center">
-            <p className="text-white mt-2">Date:</p>
-            <p className="green-text">12/23/2020</p>
-            <p className="text-white">Duration:</p>
-            <p className="green-text">1 hour</p>
-            <p className="green-text text-decoration-underline">See Full Workout</p>
-          </div>
-        </div>
-      </div>
-      <button className="position-fixed bottom-0 start-50 translate-middle-x my-3 green-button w-75 fs-2 py-2">
-            Log a Workout
-      </button>
+      {this.journalFull()}
     </div>
     </>
     );
@@ -38,3 +57,30 @@ class JournalPage extends React.Component {
 }
 
 export default JournalPage;
+/*
+<div className="container mt-3">
+      <div className="row justify-content-evenly">
+        <div className="col">
+          <div className="pop-out-colors lh-1 text-center">
+            <p className="text-white mt-2">Date:</p>
+            <p className="green-text">12/23/2020</p>
+            <p className="text-white">Duration:</p>
+            <p className="green-text">1 hour 30 minutes</p>
+            <p className="mb-1 text-light text-decoration-underline">See Full Workout</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="pop-out-colors wh-75 lh-1 text-center">
+            <p className="text-white mt-2">Date:</p>
+            <p className="green-text">12/20/2020</p>
+            <p className="text-white">Duration:</p>
+            <p className="green-text">1 hour</p>
+            <p className="mb-1 text-light text-decoration-underline">See Full Workout</p>
+          </div>
+        </div>
+      </div>
+      <button className="position-fixed bottom-0 start-50 translate-middle-x my-3 green-button w-75 fs-2 py-2">
+            Log a Workout
+      </button>
+    </div>
+    */
