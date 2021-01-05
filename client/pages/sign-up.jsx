66180f66a9eb0a9bcc2ee5311440f98aa1632722
Rachel Beacham <rachelbeacham17@gmail.com>
2021-01-05 23:55:12 +0000

    Add a handleChange method to the sign up form that updates state with user info. #25

--- client/pages/sign-up.jsx
+++ client/pages/sign-up.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import Header from '../components/header';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <>
@@ -10,13 +22,13 @@ class SignUp extends React.Component {
         <form>
           <div className="row justify-content-center">
             <label htmlFor="email" className="my-1 w-75 fs-4 text-start text-white">Email</label>
-            <input required type="email" name="email"
+            <input onChange={this.handleChange} required type="email" name="email"
               className="mb-3 gray-text w-75 pop-in-colors d-block"></input>
               <label htmlFor="username" className="my-1 w-75 fs-4 text-start text-white">Username</label>
-            <input required type="text" name="username"
+            <input required onChange={this.handleChange} type="text" name="username"
               className="mb-3 w-75 gray-text pop-in-colors d-block"></input>
             <label htmlFor="password" className="my-1 fs-4 w-75 text-start text-white">Password</label>
-            <input required type="password" name="password"
+            <input required onChange={this.handleChange} type="password" name="password"
               className="mb-3 w-75 gray-text pop-in-colors d-block"></input>
             <button type="submit"
                 className="my-3 green-button w-90 fs-2 py-2 position-fixed bottom-0 start-50 translate-middle-x">
