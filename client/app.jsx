c458ebfc005a9be700253d297ba340fda43a5d65
Rachel Beacham <rachelbeacham17@gmail.com>
2021-01-05 20:40:33 +0000

    Implement hash routing. #25

--- client/app.jsx
+++ client/app.jsx
@@ -5,8 +5,24 @@ import AddExercise from './components/add-exercise';
 import JournalPage from './pages/journal';
 import CreateProfileForm from './components/create-profile-form';
 import LandingPage from './pages/landing-page';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: parseRoute(window.location.hash)
+    };
+  }
+
+  conmponentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const location = parseRoute(window.location.hash);
+      this.setState({
+        location
+      });
+    });
+  }
 
   render() {
     return <LandingPage />;
--- client/pages/landing-page.jsx
+++ client/pages/landing-page.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 class LandingPage extends React.Component {
   render() {
     return (
-      <div className="flex-column text-center">
+      <div className="text-center">
         <div className="landing-image"></div>
         <div className="landing-buttons">
-          <button className="pop-in-colors mb-3 py-2 w-75 text-white fs-2">Login</button>
-          <button className="pop-in-colors my-3 py-2 w-75 text-white fs-2">Sign Up</button>
+          <button className="pop-in-colors mb-3 py-2 w-75 text-white fs-2">
+            Login
+            <a href="#login"></a>
+            </button>
+          <button className="pop-in-colors my-3 py-2 w-75 text-white fs-2">
+            Sign Up
+            <a href="#sign-up"></a>
+            </button>
         </div>
       </div>
     );
