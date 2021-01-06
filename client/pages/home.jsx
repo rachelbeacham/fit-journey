eb57f528cad4378446a717247ecd32e8a4655718
Rachel Beacham <rachelbeacham17@gmail.com>
2021-01-06 06:06:53 +0000

    Completed issue-27. #27

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -19,20 +19,3 @@ export default function Home(props) {
     </>
   );
 }
-/*
- <div className="container text-center">
-        <div className="d-flex flex-column justify-content-center">
-          <a href="log-workout">
-            <div className="log-workout my-4 home-image text-white fs-4 d-block w-75">Log Workout</div>
-          </a>
-          <a href="view-exercises">
-            <div className="view-exercises  my-4 home-image text-white fs-4 w-75">View Exercises</div>
-          </a>
-        </div>
-        <div className="pop-out-colors d-flex my-3 w-90 py-3 position-fixed bottom-0 start-50 translate-middle-x">
-          <a href='profile'><i className="fas col fa-user fs-1"></i></a>
-          <a href='home'><i className="fas col fa-dumbbell fs-1"></i></a>
-          <a href='journal'><i className="fas col fa-book fs-1"></i></a>
-        </div>
-      </div>
-*/
