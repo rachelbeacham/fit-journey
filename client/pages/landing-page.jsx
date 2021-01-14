88bd13486d337bb1f7520ef69d8f77e17e4e42bf
Rachel Beacham <rachelbeacham17@gmail.com>
2021-01-14 03:30:11 +0000

    Add a button to landing page that says sign in with demo account. #40

--- client/pages/landing-page.jsx
+++ client/pages/landing-page.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      demo: false,
+      username: '',
+      password: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      demo: true,
+      username: 'demoUser',
+      password: 'password'
+    });
+  }
+
   render() {
     return (
       <div className="text-center">
@@ -16,6 +34,9 @@ class LandingPage extends React.Component {
             Sign Up
             </button>
           </a>
+          <button className="pop-in-colors my-3 py-2 w-75 text-white fs-2" onClick={this.handleClick}>
+              Skip Login & Demo!
+          </button>
         </div>
       </div>
     );
